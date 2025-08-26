93beb0d58cbbd53b4949050eaf87bf5e554b93c6
sundaytuesday <micahh676@gmail.com>
2025-08-26 08:07:50 -0700

    Update main.java

--- main.java
+++ main.java
@@ -1,6 +1,16 @@
 public class main {
-
     public static void main(String[] args) {
-        System.out.println("Hello World")
+        int a = 10, b = 3, t;
+        for (int i=1; i<=6; i++)
+        {
+        t = a;
+        a = i + b;
+        b = t - i;
+        System.out.print(t);
+        System.out.print(a);
+        System.out.println(b);
+
+        }
+
     }
 }
