957839f7fe85341962ccb36ce3c6ffabfbf42ad9
sundaytuesday <micahh676@gmail.com>
2025-08-26 08:14:34 -0700

    Update main.java

--- main.java
+++ main.java
@@ -1,35 +1,5 @@
 public class main {
     public static void main(String[] args) {
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-        int a = 10, b = 3, t;
-        for (int i=1; i<=6; i++)
-        {
-        t = a;
-        a = i + b;
-        b = t - i;
-        System.out.print(t);
-        System.out.print(a);
-        System.out.println(b);
-
+        System.out.println("Hello World");
         }
-
-=======
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-        for (int i=0; i < 10; i++) {
-            System.out.println("Hello World");
-        }
-            
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
     }
-}
